8bf3a703f59139d24158feb941f59edfbf30de8e
ross-crawford <rcrawford90@hotmail.co.uk>
2020-03-23 10:34:14 +0000

    recycler view implemented with data display

--- app/src/main/java/com/rosscrawford/mpdtrafficscotland/ItemList.java
+++ app/src/main/java/com/rosscrawford/mpdtrafficscotland/ItemList.java
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Adapter;
 import android.widget.EditText;
 
 public class ItemList extends AppCompatActivity {
